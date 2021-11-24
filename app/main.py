506662d93b64086f1c65f54a427d35a62c388e96
diebautistat <diego.bautistatorres@mercadolibre.com.co>
2021-11-24 11:39:55 -0500

    Se restauran los cambios hechos para laboratorio.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict
+from Crypto import PublicKey
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
 from base64 import b64decode, b64encode
 import openpyxl
 import requests
@@ -16,21 +16,8 @@ def write_to_file(file_name: str, data: Dict) -> None:
     pass
 
 def encrypt_phone(phone: str) -> str:
-    url = f"http://localhost:8080/hash/phone/{phone}"
-    response = requests.get(url)
-    return response.text
+    pass
+
 
 if __name__ == '__main__':
-    phones_file = openpyxl.load_workbook('Phones.xlsx')
-    sheet = phones_file["Base definitiva"]
-    rows_count = sheet.max_row
-    for i in range (2, rows_count + 1):
-        phone = f"F{i}"
-        phone_initial_value = f"H{i}"
-        phone_final_value = f"I{i}"
-        initial_value = str(sheet[phone].value).lstrip('0')[-10:]
-        encrypted_phone =  encrypt_phone(initial_value)
-        sheet[phone_initial_value].value = initial_value
-        sheet[phone_final_value].value = encrypted_phone
-        print(i)
-    phones_file.save("Output.xlsx")
+    pass
---
+++ tests/test_script.py
@@ -0,0 +1,20 @@
+from app.main import encrypt_phone
+
+# def test__encrypt__on_any_input__output_should_be_different_than_input():
+#     input = "any_string"
+#     output = encrypt(input)
+#     assert input != output
+
+# def test__encrypt__when_input_is_hello_return_should_be_0984502ihfpwruhfo459uf():
+#     input = "hello"
+#     output = encrypt(input)
+#     assert output == "0984502ihfpwruhfo459uf"
+
+
+# para probar la función encrypt que hace uso de un servicio web para el cifrado se debe probar es que la función haga el llamado http correspondiente
+def test_any():
+    input = "any"
+    #_ = encrypt_phone(input)
+    assert True
+
+
