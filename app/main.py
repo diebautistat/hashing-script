dc5b79cdc2024a78db0577dcd0b189657b221c21
diebautistat <diego.bautistatorres@mercadolibre.com.co>
2021-12-01 11:23:13 -0500

    Se implementa el llamado al servicio y la lectura del archivo usando pandas y TDD.
    Co-authored-by: Angela Julieth Hernández Cáceres <julieth.hernandez@mercadolibre.com.co>
    Co-authored-by: Julián Osorio <juliane.osorio@mercadolibre.com.co>

--- app/main.py
+++ app/main.py
@@ -1,22 +1,25 @@
 from typing import Any, Dict
 import pandas as pd
+import requests
 
     
 
 def read_file(file_name: str) -> Dict:
     df = pd.read_excel(file_name)
     df["AC + PN"] = df["AC + PN"].apply(lambda x: str(x)[-10:])
-    dict_columns = {"F"+ str(k + 2):v for k, v in df["AC + PN"].to_dict().items()}
-    return dict_columns
+    return {"F"+ str(k + 2):v for k, v in df["AC + PN"].to_dict().items()}
 
 def encrypt_values(rows: Dict) -> Dict:
-    pass
+    return {"H" + k[1:]:encrypt_phone(v) for k, v in rows.items()}
 
 def write_to_file(file_name: str, data: Dict) -> None:
     pass
 
 def encrypt_phone(phone: str) -> str:
-    pass
+    response = requests.get("http://localhost:8080/hash/phone/" + phone)
+    if response:        
+        return response.text
+    return "" #TODO Catch exceptions and act accordingly
 
 
 if __name__ == '__main__':
--- tests/test_script.py
+++ tests/test_script.py
@@ -1,4 +1,7 @@
-from app.main import read_file
+from app.main import read_file, encrypt_values, encrypt_phone
+import pytest
+import requests
+
 
 
 def test__read_file__after_file_reading__output_cardinal_should_be_103949():
@@ -11,3 +14,20 @@ def test__read_file__after_file_reading__output_dict_should_hold_F_column():
     output = read_file(input)
     assert output["F7"] == "04811118843"[-10:]
 
+
+def test__encrypt_values___a_phone_needs_to_be_encrypted__encrypt_function_should_be_called(mocker):
+    output = {"F2": "1234567890"}
+    instance = mocker.patch("app.main.encrypt_phone")
+    encrypted_rows = encrypt_values(output)
+    instance.assert_called_with("1234567890")
+
+def test__encrypt_values___phone_to_encrypt_is_passed_as_parameter__response_contains_encrypted_phone(mocker):
+    output = {"F2": "1234567890"}
+    mocker.patch("app.main.encrypt_phone", return_value = "LOPOJSNCU")
+    encrypted_rows = encrypt_values(output)
+    assert encrypted_rows["H2"]  == "LOPOJSNCU"
+
+def test__encrypt_phone__phone_is_encrypted__encrypt_endpoint_should_be_called(mocker):
+    instance = mocker.patch("requests.get")
+    encrypt_phone("1234567890")
+    instance.assert_called_with("http://localhost:8080/hash/phone/1234567890")
